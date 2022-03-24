fe5fd4cb51bb0893bbb91cf5b667da0affc37a5c
Fausto Soares <41522884+faustosm@users.noreply.github.com>
2022-03-24 17:04:23 -0300

    Update pos-formtacao.sh

--- pos-formtacao.sh
+++ pos-formtacao.sh
@@ -72,6 +72,29 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 echo "----------------------------------------------------------------------------------------------------------"
+printf "| :: Instação Krew 'Kubernetes' :: |\n\n"
+
+# Link abaixo para a documentação e instalação do Krew
+# https://krew.sigs.k8s.io/docs/user-guide/setup/install/
+
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+
+echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> .zshrc
+
+source .zshrc
+
+kubectl krew install ns ctx
+
+
+echo "----------------------------------------------------------------------------------------------------------"
 printf "| :: Postman :: |\n\n"
 
 sudo snap install postman
